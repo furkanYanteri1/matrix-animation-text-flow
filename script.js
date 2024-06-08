bdebb28c2e5ce27b92bd29c7a005bde8263d12df
Furkan Yanteri <furkanyanteri@gmail.com>
2024-06-08 19:47:47 +0300

    feat: drawer implemented, stage 1 is completed

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ function createTextNode(letter) {
     return span;
 }
 
-// TODO, create the drawer using createTextNode
+function draw() {
+    body.innerHTML = '';
+    body.style.cssText = 'margin: 0; overflow: hidden; background: gray; color: #0F0; font-family: monospace; white-space: nowrap; line-height: 1.2em; font-size: 16px; position: relative;';
+    
+    for (let i = 0; i < drops.length; i++) {
+        const text = letters[Math.floor(Math.random() * letters.length)];
+        const textNode = createTextNode(text);
+        textNode.style.position = 'absolute';
+        textNode.style.top = `${drops[i] * fontSize}px`;
+        textNode.style.left = `${i * fontSize}px`;
+        body.appendChild(textNode);
+
+        if (drops[i] * fontSize > window.innerHeight && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 33);
